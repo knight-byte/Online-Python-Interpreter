81f51cffff8b6d8bea60368b4e4f1ed69a977425
abunachar <abunachar1236@gmail.com>
2020-09-20 20:10:59 +0530

    class to function view

--- pages/views.py
+++ pages/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
 
-# from django.views.generic import TemplateView, ListView
 from .models import Post
-
-
-# class HomePageView(TemplateView):
-#     template_name = "home.html"
-
-
-# class CompilerPageView(TemplateView):
-#     template_name = "compiler.html"
-
-
-# class LearnListView(ListView):
-#     model = Post
-#     template_name = 'learn.html'
 
 
 def HomePageView(response):
