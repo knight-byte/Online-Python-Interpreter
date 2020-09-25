f990adfbb34ed8f172b3006d3fa746ddcb6ec527
Knight-byte <abunachar1236@gmail.com>
2020-09-25 21:41:45 +0530

    Register Form Function View added

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 
+from django.contrib import messages
 from .models import Post
+from .forms import UserRegisterform
 
 
 def HomePageView(response):
@@ -16,3 +21,22 @@ def LearnPageView(response):
         'Post': Post.objects.all()
     }
     return render(response, 'learn.html', content)
+
+
+def Register(request):
+    if request.method == 'POST':
+        form = UserRegisterform(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request, f'Your Account has been Created! You Can Login')
+            return redirect('login')
+    else:
+        form = UserRegisterform()
+    return render(request, 'Signup.html', {'form': form})
+
+
+@login_required
+def Profile(request):
+    return render(request, 'profile.html')
