166f86f619ac0e5fd3febd4f813d1a44196c69d0
Knight-byte <abunachar1236@gmail.com>
2020-09-25 21:43:32 +0530

    login,signup,profile url added

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView, name='home'),
     path('compile/', views.CompilerPageView, name='compile'),
-    path('learn/', views.LearnPageView, name='learn')
+    path('learn/', views.LearnPageView, name='learn'),
+    path('Signup/', views.Register, name='register'),
+    path('profile/', views.Profile, name='profile'),
 
 ]
