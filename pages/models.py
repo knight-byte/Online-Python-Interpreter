5f3ed90c8bdf37dc1536259e11fddae4685a80b5
Knight-byte <abunachar1236@gmail.com>
2020-10-02 21:30:58 +0530

    Beginner and Advance model created

--- pages/models.py
+++ pages/models.py
@@ -1,19 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 from PIL import Image
+from django.urls import reverse
 
 
-class Post(models.Model):
+class BegPost(models.Model):
 
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE,
     )
-    body = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    solution = models.TextField()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('beg-post-detail', kwargs={'pk': self.pk})
+
+
+class AdvPost(models.Model):
+
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    date_posted = models.DateTimeField(default=timezone.now)
+    solution = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('adv-post-detail', kwargs={'pk': self.pk})
 
 
 class Profile(models.Model):
